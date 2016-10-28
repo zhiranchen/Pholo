48aa5b8939d41c6626e55439c03eba26e4f77d9a
Michael Chuang <michael.a.chuang@gmail.com>
2016-10-27 20:10:40 -0700

    Updated navigation bar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+  res.sendfile('index.html');
 });
 
 app.listen(3000, function () {
